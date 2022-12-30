17d26e46d05e2a1a4b72cd653ed32830fc07a848
Anaveragec0der <105019665+Anaveragec0der@users.noreply.github.com>
2022-12-30 19:17:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -222,6 +222,6 @@ app.get("/:customListName",function(req,res){
 
 
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log("Server started successfully");
 });
